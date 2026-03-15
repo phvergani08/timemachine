6ab2e49ff6b93189d74dade5f110a20c6a4ffca6
Pedro Henrique <pedrohenriquevn08@hotmail.com>
2026-03-15 19:41:46 -0300

    Add files via upload

--- main.c
+++ main.c
@@ -1,12 +1,10 @@
  /* CREATING A TIME MACHINE
   * - The file consists in questions of a txt file.
-  * - The answers will be storaged in an txt files and discovered 1 year after.
-  */
+  * - The answers will be storaged in an txt files and discovered 1 year after. */
 
 #include <stdio.h>
 #include <string.h>
 #include <time.h>
-
  int main() {
      // START APP
      printf("Welcome to the time machine!\n\n");
@@ -22,14 +20,17 @@
     char answer2[200];
     char answer3[200];
 
+    // First question
     fgets(questions, 80, fptr);
     printf("%s\n", questions);
     fgets(answer1, sizeof(answer1), stdin); // fgets instead of scanf
 
+    // Second Question
     fgets(questions, 80, fptr);
     printf("%s\n", questions);
     fgets(answer2, sizeof(answer2), stdin);
 
+    // third question
     fgets(questions, 80, fptr);
     printf("%s\n", questions);
     fgets(answer3, sizeof(answer3), stdin);
@@ -44,9 +45,9 @@
     FILE *fpty;
 
     fpty = fopen("answers.txt", "w");
-    fprintf(fpty, "%s\n", answer1);
-    fprintf(fpty, "%s\n", answer2);
-    fprintf(fpty, "%s\n", answer3);
+    fprintf(fpty, "%s", answer1);
+    fprintf(fpty, "%s", answer2);
+    fprintf(fpty, "%s", answer3);
 
     fclose(fpty);
  }
